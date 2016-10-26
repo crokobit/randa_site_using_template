cda7238cf36897da6a8590718962ff0da0a7a74e
Ivan <crokobit@gmail.com>
2016-10-26 23:42:42 +0800

    article show, index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,35 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(safe_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      flash[:error] == @articles.errors
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def safe_params
+    params.require(:article).permit(:title, :content)
   end
 end
