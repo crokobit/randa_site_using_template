650e168d06c13c536b6393fb855ba02b95169a52
Ivan <crokobit@gmail.com>
2016-10-27 00:02:54 +0800

    article CRUD done execpt D

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
+    @article = Article.find(params[:id])
+    if @article.update(safe_params)
+      redirect_to articles_path
+    else
+      flash[:error] == @articles.errors
+      render :new
+    end
   end
 
   def destroy
